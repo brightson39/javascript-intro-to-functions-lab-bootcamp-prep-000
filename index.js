0f307bf451bd345ee62c8e0477ee00f0845d5f8b
Bright Olayinka <explorebright@gmail.com>
2019-02-28 19:54:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ function whisper(string){
   return string.toLowerCase()
 }
 
-function logShout(string){
-  console.log(string).toUpperCase()
-}
+function whisper(string){
+  console.log(`$string`)
+
+function sayHiToGrandma(string){
+  var
+  console.log("I can't hear you!")
+  if string
 
